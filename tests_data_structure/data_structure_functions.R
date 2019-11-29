84c0f601a0fe0961e46c262c7b0dba17bbd7624f
ThomasRobine <45353523+ThomasRobine@users.noreply.github.com>
2019-11-29 16:59:43 +0100

    update files

--- tests_data_structure/data_structure_functions.R
+++ tests_data_structure/data_structure_functions.R
@@ -11,20 +11,6 @@ NPA_list = function(repetition,struct) {
   return(li)
 }
 
-# return a non pre-allocated list which contains a certain time (repetition) 
-# the same list or class instanciation (struct)
-# this time the copy_on_modifying is add (we change some elements inside struct)
-# WCOM stands for With Copy On Modifying
-NPA_list_WCOM = function(repetition,struct) {                                                     
-  li = list()
-  struct$name <- "rouge gorge"
-  struct$date_begin = 3102019
-  for (i in 1:repetition) {
-    li = append(li,struct)
-  }
-  return(li)
-}
-
 # return a pre allocated vector which contains a certain time (repetition)
 # the same list or class instanciation (struct)
 # PA stands for Pre-allocated
@@ -46,32 +32,6 @@ PA_vector_Df_and_Tb = function(repetition,struct) {
   return(rbindlist(vec))
 }
 
-# return a pre-allocated vector which contains a certain time (repetition) 
-# the same list or class instanciation (struct)
-# this time the copy_on_modifying is add (we change some elements inside struct)
-PA_vector_list_and_class_WCOM = function(repetition,struct) {                                    
-  vec = vector("list",repetition)
-  struct$name <- "rouge gorge"
-  struct$date_begin = 3102019
-  for (i in 1:repetition) {
-    vec[[i]] = struct
-  }
-  return(vec)
-}
-
-# Return a pre allocated vector which contains a certain time (repetition)
-# the same data frame or tibble (struct)
-# This time the copy_on_modifying is add (we change some elements inside struct)
-PA_vector_Df_and_Tb_WCOM = function(repetition,struct) {                                                    
-  vec = vector("list",repetition)
-  struct$name <- "rouge gorge"
-  struct$date_begin = 3102019
-  for (i in 1:repetition) {
-    vec[[i]] = struct
-  }
-  return(rbindlist(vec))
-}
-
 # return a pre allocated list which contains a certain time (repetition)
 # the same list or class instanciation (struct)
 PA_List_list_and_class = function(repetition,struct) {                                                    
@@ -82,26 +42,6 @@ PA_List_list_and_class = function(repetition,struct) {
 # return a pre allocated list which contains a certain time (repetition)
 # the same data frame or tibble (struct)
 PA_List_Df_and_Tb = function(repetition,struct) {                                                    
-  li <- lapply(1:repetition,function(i) struct)                                          
-  return(rbindlist(li))
-}
-
-# return a pre allocated list which contains a certain time (repetition)
-# the same list or class instanciation (struct)
-# This time the copy_on_modifying is add (we change some elements inside struct)
-PA_List_list_and_class_WCOM = function(repetition,struct) {       
-  struct$name <- "rouge gorge"
-  struct$date_begin = 3102019
-  li <- lapply(1:repetition,function(i) struct)                                          
-  return(li)
-}
-
-# return a pre allocated list which contains a certain time (repetition)
-# the same data frame or tibble (struct)
-# This time the copy_on_modifying is add (we change some elements inside struct)
-PA_List_Df_and_Tb_WCOM = function(repetition,struct) {    
-  struct$name <- "rouge gorge"
-  struct$date_begin = 3102019
   li <- lapply(1:repetition,function(i) struct)                                          
   return(rbindlist(li))
 }
