2a7428e7a71cd3eef19aae09d89cad2c417bdc90
ThomasRobine <45353523+ThomasRobine@users.noreply.github.com>
2020-01-14 19:28:41 +0100

    modifications ref classes functions

--- tests_data_structure/data_structure_functions.R
+++ tests_data_structure/data_structure_functions.R
@@ -3,7 +3,7 @@
 # return a non pre-allocated list which contains a certain time (repetition) 
 # the same list or class instanciation (struct)
 # NPA stands for Non Pre-Allocated
-NPA_list = function(repetition,struct) {                                                     
+NPA_list_list = function(repetition,struct) {                                                     
   li = list()
   for (i in 1:repetition) {
     li = append(li,struct)
@@ -11,16 +11,37 @@ NPA_list = function(repetition,struct) {
   return(li)
 }
 
+
+# return a non pre-allocated list which contains a certain time (repetition) 
+# the same list or class instanciation (struct)
+# NPA stands for Non Pre-Allocated
+NPA_list_class = function(repetition,struct) {                                                     
+  li = list()
+  for (i in 1:repetition) {
+    li = append(li,struct$copy())
+  }
+  return(li)
+}
+
+
 # return a pre allocated vector which contains a certain time (repetition)
 # the same list or class instanciation (struct)
 # PA stands for Pre-allocated
-PA_vector_list_and_class = function(repetition,struct) {
-  #print(s#printf("%s %s","tracemem struct : ",tracemem(struct)))
+PA_vector_list = function(repetition,struct) {
   vec = vector("list",repetition)
-  #print(s#printf("%s %s","tracemem vect : ",tracemem(vec)))
   for (i in 1:repetition) {
     vec[[i]] = struct
-    #print(s#printf("%s %s","tracemem vect : ",tracemem(vec)))
+  }
+  return(vec)
+}
+
+# return a pre allocated vector which contains a certain time (repetition)
+# the same list or class instanciation (struct)
+# PA stands for Pre-allocated
+PA_vector_class = function(repetition,struct) {
+  vec = vector("list",repetition)
+  for (i in 1:repetition) {
+    vec[[i]] = struct$copy()
   }
   return(vec)
 }
@@ -28,28 +49,31 @@ PA_vector_list_and_class = function(repetition,struct) {
 # return a pre allocated vector which contains a certain time (repetition)
 # the same data frame or tibble (struct)
 PA_vector_Df_and_Tb = function(repetition,struct) {  
-  #print(s#printf("%s %s","tracemem struct ",tracemem(struct)))
   vec = vector("list",repetition)
-  #print(s#printf("%s %s","tracemem vec 1 ",tracemem(vec)))
   for (i in 1:repetition) {
     vec[[i]] = struct
-    #print(s#printf("%s %s","tracemem vec 2 ",tracemem(vec)))
   }
   return(rbindlist(vec))
 }
 
 # return a pre allocated list which contains a certain time (repetition)
 # the same list or class instanciation (struct)
-PA_List_list_and_class = function(repetition,struct) {  
-  #print(s#printf("%s %s","tracemem struct ",tracemem(struct)))
+PA_List_list = function(repetition,struct) {  
   li <- lapply(1:repetition,function(i) struct)                                          
+  return(li)
+}
+
+
+# return a pre allocated list which contains a certain time (repetition)
+# the same list or class instanciation (struct)
+PA_List_class = function(repetition,struct) {  
+  li <- lapply(1:repetition,function(i) struct$copy())                                          
   return(li)
 }
 
 # return a pre allocated list which contains a certain time (repetition)
 # the same data frame or tibble (struct)
 PA_List_Df_and_Tb = function(repetition,struct) { 
-  #print(s#printf("%s %s","tracemem struct ",tracemem(struct)))
   li <- lapply(1:repetition,function(i) struct)                                          
   return(rbindlist(li))
 }
