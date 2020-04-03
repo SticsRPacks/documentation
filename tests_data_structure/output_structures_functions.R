a51191fa814f7382ae6b66f4b7900c586bbe2f18
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-04-03 15:58:05 +0200

    separation of functions and tests, added functions alternatives, comments and questions. Formatting simplification ito RMD file

---
+++ tests_data_structure/output_structures_functions.R
@@ -0,0 +1,205 @@
+
+
+################### DATA STRUCTURES CREATION ############################
+
+# Date = function(size) {
+#   return(structure(rep(NA_real_,size),class="POSIXct"))
+# }
+
+# fonction qui cr?e l'alternative tibble
+# elle prend en param?tres :
+# _ USM_list qui correspond au vecteur de noms des USMS, ici c'est USM_list_1996
+# _ doe_size qui correspond ? la taille du DoE, ici c'est 2
+# _ usm_number qui est le nombre d'USMS diff?rentes, ici 6
+# _ stics_inputs_path pour get_daily_result
+create_tibble = function(USM_list,doe_size,usm_number,sim_data) {
+  tb <<- tibble(Name=character(),DoE=numeric(),Date=Date(),Jul=numeric(),
+                Lai_n=double(),Masec_n=double(),Mafruit=double(),Hr_1=double(),
+                Hr_2=double(),Hr_3=double(),Hr_4=double(),Hr_5=double(),Resmes=double())
+  lapply(1:doe_size, function(doe) {
+    lapply(USM_list, function(usm) {
+      #tibble_res = SticsRFiles::get_daily_results(file.path(stics_inputs_path, USM_list[usm]), USM_list[usm])
+      tibble_res = sim_data[[usm]]
+      # tibble_res = dplyr::select(tibble_res,Date,jul,lai_n,masec_n,mafruit,HR_1,HR_2,HR_3,HR_4,HR_5,resmes)
+      # tibble_res = dplyr::filter(tibble_res,Date >= ymd("1996/01/01") & Date <= ymd("1996/10/15"))
+      Name = rep(NA,nrow(tibble_res))
+      DoE = rep(doe,nrow(tibble_res))
+      tibble_res = cbind(Name,DoE,tibble_res)
+      lapply(1:(usm_number/length(USM_list)), function(id) {
+        Name = rep(paste(usm,"_",id,sep=""),nrow(tibble_res))
+        tibble_res[,1] = Name
+        tb <<- rbind(tb,tibble_res)
+      })
+    })
+  })
+  # Patrice
+  # changing tibble to as_tibble 
+  return(as_tibble(tb))
+}
+
+
+
+
+
+create_tibble2 = function(USM_list,doe_size,usm_number,sim_data) {
+  # USM_list: vecteur des noms d'usm (parmi les noms de la liste sim_data) 
+  # doe_size: nombre d'échantillons du doe
+  # usm_number: nombre d'usm à générer à partir des usms de USM_list
+  # sim_data: list de tibbles retournée par la fonction stics_wrapper 
+  # avec sélection des colonnes et filrage des ligne sur une plage de dates
+  # au préalable (voir dans TESTS plus bas)
+  
+  # replicating tibbles: each sim_data element by generated usm names 
+  usms_nb <- length(USM_list)
+  usm_names_nb <- usm_number/usms_nb
+  #
+  one_doe_tb <- lapply(1:usms_nb, function(x) {
+    usm_sub_list <- rep(sim_data[x],usm_names_nb)
+    names(usm_sub_list) <- paste(rep(USM_list[x],usm_names_nb), 1:usm_names_nb, sep="_")
+    bind_rows(usm_sub_list, .id = "Name")
+  })
+  
+  # Replicating one_doe_tb doe_size times; conversion to tibble
+  tb <- bind_rows(rep(one_doe_tb, doe_size))
+  # adding doe identifiers to tb
+  tb$DoE = sort(rep(1:doe_size,nrow(tb)/doe_size))
+  
+  return(tb)
+}
+
+
+create_list = function(USM_list,doe_size,usm_number,sim_data) {
+  li <- vector("list",doe_size)
+  lapply(1:doe_size, function(doe) {
+    cpt = 1
+    li2 = vector("list",length(USM_list)*floor(usm_number/length(USM_list)))
+    names_usm = vector("list",length(USM_list)*floor(usm_number/length(USM_list)))
+    lapply(1:length(USM_list), function(usm) {
+      #tibble_res = SticsRFiles::get_daily_results(file.path(stics_inputs_path, USM_list[usm]), USM_list[usm])
+      tibble_res = sim_data[[usm]]
+      tibble_res = dplyr::select(tibble_res,Date,jul,lai_n,masec_n,mafruit,HR_1,HR_2,HR_3,HR_4,HR_5,resmes)
+      tibble_res = dplyr::filter(tibble_res,Date >= ymd("1996/01/01") & Date <= ymd("1996/10/15"))
+      lapply(1:(usm_number/length(USM_list)), function (id) {
+        Name = paste(USM_list[usm],"_",id,sep="")
+        names_usm[[cpt]] <<- Name
+        li2[[cpt]] <<- tibble_res[,]
+        cpt <<- cpt+1
+      })
+    })
+    names(li2) <- names_usm
+    li[[doe]] <<- li2
+  })
+  names(li) <- 1:doe_size
+  return(li)
+}
+
+# Alternative: r
+create_list2 = function(USM_list,doe_size,usm_number,sim_data) {
+  # USM_list: vecteur des noms d'usm (parmi les noms de la liste sim_data) 
+  # doe_size: nombre d'échantillons du doe
+  # usm_number: nombre d'usm à générer à partir des usms de USM_list
+  # sim_data: list de tibbles retournée par la fonction stics_wrapper 
+  # avec sélection des colonnes et filrage des ligne sur une plage de dates
+  # au préalable (voir dans TESTS plus bas)
+  
+  # replicating list: each sim_data element by generated usm names 
+  usms_nb <- length(USM_list)
+  usm_names_nb <- usm_number/usms_nb
+  #
+  one_doe_list <- lapply(1:usms_nb, function(x) {
+    usm_sub_list <- rep(sim_data[x],usm_names_nb)
+    names(usm_sub_list) <- paste(rep(USM_list[x],usm_names_nb), 1:usm_names_nb, sep="_")
+    usm_sub_list
+  })
+  
+  # Replicating one_doe_list doe_size times; adding doe names
+  one_doe_list <- unlist(one_doe_list, recursive = FALSE)
+  li <- lapply(1:doe_size, function(x) return(one_doe_list))
+  # set doe identifiers as names
+  names(li) <- as.character(1:doe_size)
+  
+  return(li)
+}
+
+################# EXTRACTION ################################
+# Patrice
+# ne fonctionne que pour une usm, generalisation a une liste ?
+# On perd les infos de départ sur usm, doe
+# Est-ce qu'il ne faudrait pas renvoyer un objet du même type que celui de départ ?
+list_get_dates_and_var_values = function(structure,doe,usm_name,var) {
+  structure[[doe]][[usm_name]][,c("Date",var)]
+}
+
+# voir si ok pour une liste d'usm
+# On perd les infos de départ sur usm, doe
+# Est-ce qu'il ne faudrait pas renvoyer un objet du même type que celui de départ ?
+tibble_get_dates_and_var_values = function(structure,doe,usm_name,var) {
+  var <- enquo(var)
+  dplyr::filter(structure, DoE == doe,Name == usm_name) %>% dplyr::select(Date,!!var)
+}
+
+########################################
+
+list_get_usm_names_and_var_values = function(structure,doe,var,date) {
+  names = vector("list",length(structure[[doe]]))
+  values = vector("list",length(structure[[doe]]))
+  lapply(1:length(structure[[doe]]), function(id_usm) {
+    id_var <- which(structure[[doe]][[id_usm]]$Date == ymd(date))
+    values[id_usm] <<- structure[[doe]][[id_usm]][id_var,var]
+    names[id_usm] <<- names(structure[[doe]][id_usm])
+  })
+  cbind(names,values)
+  
+  # renvoie une matrice
+}
+
+# Alternative: renvoie une liste ou un tibble
+list_get_usm_names_and_var_values2 = function(structure,doe,var,date) {
+  
+  structure_doe <- structure[[doe]]
+  
+  # renvoie une liste
+  # lapply(structure_doe, function(x) {filter(x, Date == ymd(date)) %>% select(var)})
+  
+  # renvoie un tibble
+  bind_rows(lapply(structure_doe, function(x) {filter(x, Date == ymd(date)) %>% select(var)}),.id = "Name")
+  
+}
+
+
+tibble_get_usm_names_and_var_values = function(structure,doe,var,date) {
+  var <- enquo(var)
+  dplyr::filter(structure,DoE == doe,Date == ymd(date)) %>% dplyr::select(Name,!!var)
+}
+
+#########################################
+
+list_get_DOE_and_var_values = function(structure,usm_name,var,date) {
+  DOE <- vector("list",length(structure))
+  values <- vector("list",length(structure))
+  lapply(1:length(structure), function(doe) {
+    id_usm <- which(names(structure[[doe]]) == usm_name)
+    id_var <- which(structure[[doe]][[id_usm]]$Date == ymd(date))
+    values[doe] <<- structure[[doe]][[id_usm]][id_var,var]
+    DOE[doe] <<- doe
+  })
+  cbind(DOE,values)
+}
+
+# Alternative: renvoie une liste ou un tibble
+list_get_DOE_and_var_values2 = function(structure,usm_name,var,date) {
+  
+  structure_usm <- lapply(structure, function(x) x[[usm_name]])
+  
+  # renvoie une liste
+  #lapply(structure_usm, function(x) {filter(x, Date == ymd(date)) %>% select(var)})
+  
+  # renvoie un tibble
+  bind_rows(lapply(structure_usm, function(x) {filter(x, Date == ymd(date)) %>% select(var)}),.id = "Name")
+  
+}
+
+tibble_get_DOE_and_var_values = function(structure,usm_name,var,date) {
+  var <- enquo(var)
+  dplyr::filter(structure, Name == usm_name,Date == ymd(date)) %>% dplyr::select(DoE,!!var)
+}
--- tests_data_structure/output_tests.R
+++ tests_data_structure/output_tests.R
@@ -4,164 +4,170 @@ library(data.table)
 library(lubridate)
 library(microbenchmark)
 
+
+source("output_structures_functions.R")
+
 ################### DATAS ################################
 
-# stics_inputs_path est l'emplacement du dossier V9.0/TxtFiles après avoir éxécuté RunModel.R
-stics_inputs_path = "C:/Users/Thomas/Documents/V9.0/TxtFiles"
+# stics_inputs_path est l'emplacement du dossier V9.0/TxtFiles apr?s avoir ?x?cut? RunModel.R
+#stics_inputs_path = "C:/Users/Thomas/Documents/V9.0/TxtFiles"
 
 USM_list_all = c("bo96iN+","bou00t1","bou00t3","bou99t1","bou99t3","lu96iN+",
                  "lu96iN6","lu97iN+")
 
 USM_list_1996 = c("bo96iN+","lu96iN+","lu96iN6")
 
-################### CREATION ############################
-
-Date = function(size) {
-  return(structure(rep(NA_real_,size),class="POSIXct"))
-}
-
-# fonction qui crée l'alternative tibble
-# elle prend en paramètres :
-# _ USM_list qui correspond au vecteur de noms des USMS, ici c'est USM_list_1996
-# _ doe_size qui correspond à la taille du DoE, ici c'est 2
-# _ usm_number qui est le nombre d'USMS différentes, ici 6
-# _ stics_inputs_path pour get_daily_result
-create_tibble = function(USM_list,doe_size,usm_number,stics_inputs_path) {
-  tb <<- tibble(Name=character(),DoE=numeric(),Date=Date(),Jul=numeric(),
-                Lai_n=double(),Masec_n=double(),Mafruit=double(),Hr_1=double(),
-                Hr_2=double(),Hr_3=double(),Hr_4=double(),Hr_5=double(),Resmes=double())
-  lapply(1:doe_size, function(doe) {
-    lapply(1:length(USM_list), function(usm) {
-      tibble_res = SticsRFiles::get_daily_results(file.path(stics_inputs_path, USM_list[usm]), USM_list[usm])
-      tibble_res = dplyr::select(tibble_res,Date,jul,lai_n,masec_n,mafruit,HR_1,HR_2,HR_3,HR_4,HR_5,resmes)
-      tibble_res = dplyr::filter(tibble_res,Date >= ymd("1996/01/01") & Date <= ymd("1996/10/15"))
-      Name = rep(NA,nrow(tibble_res))
-      DoE = rep(doe,nrow(tibble_res))
-      tibble_res = cbind(Name,DoE,tibble_res)
-      lapply(1:(usm_number/length(USM_list)), function(id) {
-        Name = rep(paste(USM_list[usm],"_",id,sep=""),nrow(tibble_res))
-        tibble_res[,1] = Name
-        tb <<- rbind(tb,tibble_res)
-      })
-    })
-  })
-  return(tibble(tb))
-}
-
-create_list = function(USM_list,doe_size,usm_number,stics_inputs_path) {
-  li <- vector("list",doe_size)
-  lapply(1:doe_size, function(doe) {
-    cpt = 1
-    li2 = vector("list",length(USM_list)*floor(usm_number/length(USM_list)))
-    names_usm = vector("list",length(USM_list)*floor(usm_number/length(USM_list)))
-    lapply(1:length(USM_list), function(usm) {
-      tibble_res = SticsRFiles::get_daily_results(file.path(stics_inputs_path, USM_list[usm]), USM_list[usm])
-      tibble_res = dplyr::select(tibble_res,Date,jul,lai_n,masec_n,mafruit,HR_1,HR_2,HR_3,HR_4,HR_5,resmes)
-      tibble_res = dplyr::filter(tibble_res,Date >= ymd("1996/01/01") & Date <= ymd("1996/10/15"))
-      lapply(1:(usm_number/length(USM_list)), function (id) {
-        Name = paste(USM_list[usm],"_",id,sep="")
-        names_usm[[cpt]] <<- Name
-        li2[[cpt]] <<- tibble_res[,]
-        cpt <<- cpt+1
-      })
-    })
-    names(li2) <- names_usm
-    li[[doe]] <<- li2
-  })
-  names(li) <- 1:doe_size
-  return(li)
-}
-
-################# EXTRACTION ################################
-
-list_get_dates_and_var_values = function(structure,doe,usm_name,var) {
-  structure[[doe]][[usm_name]][,c("Date",var)]
-}
-
-tibble_get_dates_and_var_values = function(structure,doe,usm_name,var) {
-  var <- enquo(var)
-  n <- names(structure)
-  tb <- structure[[n]]
-  tb <- dplyr::filter(tb,DoE == doe,Name == usm_name)
-  dplyr::select(tb,Date,!!var)
-}
-
-########################################
-
-list_get_usm_names_and_var_values = function(structure,doe,var,date) {
-  names = vector("list",length(structure[[doe]]))
-  values = vector("list",length(structure[[doe]]))
-  lapply(1:length(structure[[doe]]), function(id_usm) {
-    id_var <- which(structure[[doe]][[id_usm]]$Date == ymd_hms(date))
-    values[id_usm] <<- structure[[doe]][[id_usm]][id_var,var]
-    names[id_usm] <<- names(structure[[doe]][id_usm])
-  })
-  cbind(names,values)
-}
-
-tibble_get_usm_names_and_var_values = function(structure,doe,var,date) {
-  var <- enquo(var)
-  n <- names(structure)
-  tb <- structure[[n]]
-  tb <- dplyr::filter(tb,DoE == doe,Date == ymd(date))
-  dplyr::select(tb,Name,!!var)
-}
-
-#########################################
-
-list_get_DOE_and_var_values = function(structure,usm_name,var,date) {
-  DOE <- vector("list",length(structure))
-  values <- vector("list",length(structure))
-  lapply(1:length(structure), function(doe) {
-    id_usm <- which(names(structure[[doe]]) == usm_name)
-    id_var <- which(structure[[doe]][[id_usm]]$Date == ymd_hms(date))
-    values[doe] <<- structure[[doe]][[id_usm]][id_var,var]
-    DOE[doe] <<- doe
-  })
-  cbind(DOE,values)
-}
-
-tibble_get_DOE_and_var_values = function(structure,usm_name,var,date) {
-  var <- enquo(var)
-  n <- names(structure)
-  tb <- structure[[n]]
-  tb <- dplyr::filter(tb,Name == usm_name,Date == ymd(date))
-  dplyr::select(tb,DoE,!!var)
-}
 
 ############### TESTS ############################
 
+# Instanciation of data structures
+#
+# Get output data sub-list from stics_wrapper run (RunModel_test.R)
+# for USM_list_1996
+sim_data <- sim_before_optim$sim_list[[1]][USM_list_1996]
+
+# Selecting needed data
+sim_data <- lapply(sim_data, function(x) 
+  dplyr::select(x,Date,jul,lai_n,masec_n,mafruit,HR_1,HR_2,HR_3,HR_4,HR_5,resmes) %>% 
+    dplyr::filter(Date >= ymd("1996/01/01") & Date <= ymd("1996/10/15")))
+
+# Creating tibble and list instances
+tb <- create_tibble(USM_list_1996,2,6,sim_data)
+tb2 <- create_tibble2(USM_list_1996,2,6,sim_data)
+identical(tb, tb2)
+
+li <- create_list(USM_list_1996,2,6,sim_data)
+li2 <- create_list2(USM_list_1996,2,6,sim_data)
+identical(li, li2)
+
+
+
+
+#########################
 # fonctionne
-# tb <- create_tibble(USM_list_1996,2,6,stics_inputs_path)
-# res <- tibble_get_dates_and_var_values(tb,1,"bo96iN+_2","HR_1")
-# res
+tb_dates_val <- tibble_get_dates_and_var_values(tb,doe = 1,usm_name = "bo96iN+_2",var = "HR_1")
+
+# # A tibble: 289 x 2
+# Date                 HR_1
+# <dttm>              <dbl>
+#   1 1996-01-01 00:00:00  31.6
+# 2 1996-01-02 00:00:00  31.6
+# 3 1996-01-03 00:00:00  31.6
+# 4 1996-01-04 00:00:00  31.6
+# 5 1996-01-05 00:00:00  31.6
+# 6 1996-01-06 00:00:00  31.6
+# 7 1996-01-07 00:00:00  31.6
+# 8 1996-01-08 00:00:00  31.6
+# 9 1996-01-09 00:00:00  31.6
+# 10 1996-01-10 00:00:00  31.6
+# # â€¦ with 279 more rows
 
 # fonctionne
-# li <- create_list(USM_list_1996,2,6,stics_inputs_path)
-# li2 <- list_get_dates_and_var_values(li,1,"bo96iN+_2","HR_1")
-# li2
+li_dates_val <- list_get_dates_and_var_values(li,1,"bo96iN+_2","HR_1")
+
+# # A tibble: 289 x 2
+# Date                 HR_1
+# <dttm>              <dbl>
+#   1 1996-01-01 00:00:00  31.6
+# 2 1996-01-02 00:00:00  31.6
+# 3 1996-01-03 00:00:00  31.6
+# 4 1996-01-04 00:00:00  31.6
+# 5 1996-01-05 00:00:00  31.6
+# 6 1996-01-06 00:00:00  31.6
+# 7 1996-01-07 00:00:00  31.6
+# 8 1996-01-08 00:00:00  31.6
+# 9 1996-01-09 00:00:00  31.6
+# 10 1996-01-10 00:00:00  31.6
+# # â€¦ with 279 more rows
+
+
+# Les objets retournÃ©s sont les mÃªmes 
+identical(tb_var_val, li_var_val)
+
+# TODO: voir si les fonctions doivent retourner la mÃªme chose ! 
+
 
 #########################
 
 # fonctionne
-# tb <- create_tibble(USM_list_1996,2,6,stics_inputs_path)
-# res <- tibble_get_usm_names_and_var_values(tb,1,"HR_4","1996-01-05")
-# res
+tb_usms_val <- tibble_get_usm_names_and_var_values(tb,1,"HR_4","1996-01-05")
+
+# # A tibble: 6 x 2
+# Name       HR_4
+# <chr>     <dbl>
+#   1 bo96iN+_1   0  
+# 2 bo96iN+_2   0  
+# 3 lu96iN+_1  28.4
+# 4 lu96iN+_2  28.4
+# 5 lu96iN6_1  28.4
+# 6 lu96iN6_2  28.4
+
 
 # fonctionne
-# li <- create_list(USM_list_1996,2,6,stics_inputs_path)
-# li2 <- list_get_usm_names_and_var_values(li,1,"HR_4","1996/01/05-00/00/00")
-# li2
+li_usms_val <- list_get_usm_names_and_var_values(li,1,"HR_4","1996/01/05")
+
+# On perd le nom de la variable !
+
+# matrix
+
+# names       values  
+# [1,] "bo96iN+_1" 0       
+# [2,] "bo96iN+_2" 0       
+# [3,] "lu96iN+_1" 28.35693
+# [4,] "lu96iN+_2" 28.35693
+# [5,] "lu96iN6_1" 28.35693
+# [6,] "lu96iN6_2" 28.35693
+
+# Le type n'est pas le mÃªme qu'avec tibble_get_usm_names_and_var_values
+# et on perd le nom de la variable, alors qu'elle est dans tb_usms_val
+
+
+# comparaison des objets retournÃ©s : voir si les fonctions doivent retourner la mÃªme chose ! 
+identical(tb_usms_val, li_usms_val)
+
+
+li_usms_val2 <- list_get_usm_names_and_var_values2(li,1,"HR_4","1996/01/05")
+
+# # A tibble: 6 x 2
+# Name       HR_4
+# <chr>     <dbl>
+#   1 bo96iN+_1   0  
+# 2 bo96iN+_2   0  
+# 3 lu96iN+_1  28.4
+# 4 lu96iN+_2  28.4
+# 5 lu96iN6_1  28.4
+# 6 lu96iN6_2  28.4
+
+
 
 #########################
 
 # fonctionne
-# tb <- create_tibble(USM_list_1996,2,6,stics_inputs_path)
-# res <- tibble_get_DOE_and_var_values(tb,"bo96iN+_2","HR_3","1996-01-05")
-# res
+tb_doe_val <- tibble_get_DOE_and_var_values(tb,"bo96iN+_2","HR_3","1996/01/05")
+
+# # A tibble: 2 x 2
+# DoE  HR_3
+# <int> <dbl>
+# 1     1  26.0
+# 2     2  26.0
+
 
 # fonctionne
-# li <- create_list(USM_list_1996,2,6,stics_inputs_path)
-# li2 <- list_get_DOE_and_var_values(li,"bo96iN+_1","HR_3","1996/01/05-00/00/00")
-# li2
+li_doe_val <- list_get_DOE_and_var_values(li,"bo96iN+_1","HR_3","1996/01/05")
 
+# On perd le nom de la variable , alors qu'elle est dans tb_doe_val
+
+# matrix
+# 
+# DOE values  
+# [1,] 1   26.00001
+# [2,] 2   26.00001
+
+
+li_doe_val2 <- list_get_DOE_and_var_values2(li,"bo96iN+_1","HR_3","1996/01/05")
+
+# # A tibble: 2 x 2
+# Name   HR_3
+# <chr> <dbl>
+# 1 1      26.0
+# 2 2      26.0
